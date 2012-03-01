8f9803dcd6091ad9f1c0b0f75bd45a4f422bcfd5
Sergei Kirsanov <sergei.kirsanov@gmail.com>
2012-03-02 00:46:23 +0400

    update

--- src/main/java/com/stdy/calculator/core/Launcher.java
+++ src/main/java/com/stdy/calculator/core/Launcher.java
@@ -3,35 +3,17 @@ package com.stdy.calculator.core;
 import com.stdy.calculator.view.MainWindow;
 
 import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 
 public class Launcher {
-
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
-     */
-    private static void createAndShowGUI() {
-        new MainWindow();
-    }
-
 
     public static void main(String[] args) {
 
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                createAndShowGUI();
+                new MainWindow();
             }
         });
 
-        /*
 
-
-
-*/
     }
 }
